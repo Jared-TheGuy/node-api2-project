430e3912c2598a214963af5afb0544e7d607a2f4
Jared Bates-Baker <51772046+Jared-TheGuy@users.noreply.github.com>
2021-11-30 20:06:02 -0700

    Set up the scaffholding for the server listening and the Posts router

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,22 @@
 // implement your posts router here
+const express =  require('express')
+
+const router = express.Router()
+
+const Posts = require('./posts-model')
+
+router.get('/test', async (req, res) => {
+    try {
+        res.json({
+            message: "Yeah!"
+        })
+    }
+    catch (err) {
+        res.json({
+            message: "Your a wackamole"
+        })
+    }
+})
+
+module.exports = router
+
